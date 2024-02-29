e0f3bf22646523fa65b7466e9dd7715e0a346d96
Yannick te Kulve <738464+YannickTeKulve@users.noreply.github.com>
2024-02-29 08:55:02 +0100

    Test if this works

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,20 @@ if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
+
 if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/run/docker/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+
+if [ ! -z $INPUT_DOCKER_HOST ];
+then INPUT_OPTIONS="$INPUT_OPTIONS --host $INPUT_DOCKER_HOST"
+fi
+
+if [ ! -z $INPUT_DOCKER_HOST ];
+exec docker run -v "$INPUT_DOCKER_HOST":"$INPUT_DOCKER_HOST" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+else
+exec docker run -v "/run/docker/docker.sock":"/run/docker/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+fi
+
+
